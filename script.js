9d742481a2d5f48c54a2a8091a2507f24701f07c
unknown <bochvarovskidimitar@gmail.com>
2019-12-19 18:59:29 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+
+const breeds = document.querySelector(".breeds"),
+      breedOptions = document.querySelector(".breed-options"),
+      loader = document.querySelector(".loader"),
+      idiot = document.querySelector(".idiot");
+
+const promise = fetch(BREEDS_URL);
+  promise
+    .then(function(response) {
+      const processingPromise = response.json();
+      return processingPromise;
+    })
+    .then(function(processedResponse) {
+        const breeds = processedResponse.message,
+            breedsArray = Object.keys(breeds);
+        breedsArray.forEach(function(breed, index){
+          addBreed(breed, index);
+        });
+    });
+
+breedOptions.addEventListener("change", function() {
+  const breed = breedOptions.options[breedOptions.selectedIndex].text,
+        isZero = breedOptions.options[breedOptions.selectedIndex].value;
+        
+  if(isZero !== '0') {
+    const DOGGOS_URL = `https://dog.ceo/api/breed/${breed}/images`,
+          promise = fetch(DOGGOS_URL);
+    removeAllImages();
+    hideIdiot();
+    showLoader();
+    promise
+      .then(function(response) {
+        const processingPromise = response.json();
+        return processingPromise;
+      })
+      .then(function(processedResponse) {
+        const dogs = processedResponse.message;
+        hideLoader();
+        addImages(dogs);
+      })
+  } else {
+    removeAllImages();
+    showIdiot();
+  }
+  
+});
+
+function addBreed(breed, index) {
+  var opt = document.createElement("option");
+  opt.value = index + 1;
+  opt.innerHTML = breed;
+  breedOptions.appendChild(opt);
+}
+
+function removeAllImages() {
+  while (breeds.firstChild) {
+    breeds.removeChild(breeds.firstChild);
+  }
+}
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+function showIdiot() {
+  idiot.style.display = 'block';
+}
+
+function hideIdiot() {
+  idiot.style.display = 'none';
+}
+
+function addImages(dogs) {
+  dogs.forEach(function(dog, index) {
+    const img = document.createElement("img");
+    img.src = dog;
+    img.alt = `doggo_${index}`;
+    breeds.appendChild(img);
+  })
+}
+
+
+
